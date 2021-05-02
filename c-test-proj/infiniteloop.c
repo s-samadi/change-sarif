98483fabb80e2048033bc16ed93681f01fa479c8
Shadi Samadi <68650974+s-samadi@users.noreply.github.com>
2021-05-02 22:50:22 +1000

    Create infiniteloop.c

---
+++ c-test-proj/infiniteloop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void test(int n) {
+  int i = 0;
+  if (n <= 0) {
+    return;
+  }
+  while (1) {
+    // BAD: condition is never true, so loop will not terminate.
+    if (i == n) {
+      break;
+    }
+  }
+}
