17794b51cc8a4775c8b6f544467deaa8d8fbd7e9
Shadi Samadi <68650974+s-samadi@users.noreply.github.com>
2021-05-02 22:49:52 +1000

    Update globalvars.c

--- c-test-proj/globalvars.c
+++ c-test-proj/globalvars.c
@@ -1,19 +1,47 @@
- #define BUFFERSIZE (1024) 
- 
-// BAD: using gets 
-void echo_bad() { 
-    char buffer[BUFFERSIZE]; 
-    gets(buffer); 
-    printf("Input was: '%s'\n", buffer); 
-} 
- 
-// GOOD: using fgets 
-void echo_good() { 
-    char buffer[BUFFERSIZE]; 
-    fgets(buffer, BUFFERSIZE, stdin); 
-    printf("Input was: '%s'\n", buffer); 
-} 
- 
-int main(void) { 
-    return 0; 
-} 
+#include <stdio.h>
+#include <stdlib.h>
+
+char *copy;
+char *copy2;
+
+void copyArgv(char **argv) {
+	copy = argv[1];
+}
+
+void setCopy2(char *val) {
+	copy2 = val;
+}
+
+void printWrapper(char *str) {
+	printf(str);
+}
+
+int main(int argc, char **argv) {
+	copyArgv(argv);
+
+	// BAD: format comes from argv through copy
+	printf(copy);
+
+	// BAD: format comes from argv through copy
+	printWrapper(copy);
+
+	// GOOD: constant format
+	printf("%s", copy);
+
+	setCopy2(copy);
+
+	// BAD: format comes from argv through copy2 (that is set to copy that is set to argv[1])
+	printf(copy2);
+
+	// BAD: format comes from argv through copy2 (that is set to copy that is set to argv[1])
+	printWrapper(copy2);
+
+	// GOOD: constant format
+	printf("%s", copy2);
+
+	setCopy2("asdf");
+
+	// Should be GOOD because copy2 has value "asdf"
+	// But we flag this case because once a global variable gets tainted we mark all usages as tainted
+	printf(copy2);
+}
